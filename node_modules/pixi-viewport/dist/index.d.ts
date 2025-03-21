19a9c684651ddc9eb917f2ff6e88db9f17077968
JangHun93 <pain3992@gmail.com>
2025-03-21 18:16:27 +0900

    init

--- node_modules/pixi-viewport/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-///<reference path="./global.d.ts" />
-export * from './plugins';
-export * from './ease';
-export * from './InputManager';
-export * from './PluginManager';
-export * from './Viewport';
